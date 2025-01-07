78e42c1ea1dec57657873fa3635b4833c3a159d7
Dharmesh Patel <stranger306@gmail.com>
2025-01-07 18:14:26 +0530

    Setting api files and redis

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use App\Livewire\Admin\Users;
 use App\Livewire\Authentication\Login;
 use App\Livewire\Counter;
 use App\Livewire\Dashboard;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,4 +20,17 @@ Route::middleware(['auth:admin', 'auth.session'])->group(function () {
     Route::get('/dashboard',Dashboard::class);
     Route::get('/users',Users::class);
     Route::get('/logout',[Login::class,'logout']);
+});
+
+Route::get('/set-key/{value}',function($value){
+    echo "Drive : ".Cache::getDefaultDriver();
+    Cache::set('mvp', $value);
+    //Redis::set('mvp',$value);
+    echo "<br>Value has been set;";
+});
+
+Route::get('/get-key/',function(){
+    //$value = Redis::get('mvp');
+    $value = Cache::get('mvp');
+    echo "Value is {$value}";
 });
